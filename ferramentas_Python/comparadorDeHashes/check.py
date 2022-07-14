19271ae4b160a5c2691556b7934023dc9ab94172
jrrodrigo421 <rjr89000@gmail.com>
2022-07-14 11:49:15 -0300

    Comparador Hashe

---
+++ ferramentas_Python/comparadorDeHashes/check.py
@@ -0,0 +1,18 @@
+import hashlib
+
+arquivo1 = 'a.txt'
+arquivo2 = 'b.txt'
+
+hash1 = hashlib.new('ripemd160')
+hash1.update(open(arquivo1, 'rb').read())
+
+hash2 = hashlib.new('ripemd160')
+hash2.update(open(arquivo2, 'rb').read())
+
+if hash1.digest() != hash2.digest():
+    print(f'O arqiovo: {arquivo1} é diferente do arquivo: {arquivo2}')
+else:
+    print(f'O aqruivo: {arquivo1} é igual ao arquivo: {arquivo2}')
+
+
+
