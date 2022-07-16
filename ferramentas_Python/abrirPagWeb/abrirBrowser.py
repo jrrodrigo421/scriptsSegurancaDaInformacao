2bba327c2055df546e0f9444d22854443b1a0389
jrrodrigo421 <rjr89000@gmail.com>
2022-07-16 13:47:45 -0300

    verificador de IP externo

---
+++ ferramentas_Python/abrirPagWeb/abrirBrowser.py
@@ -0,0 +1,15 @@
+import webbrowser
+from tkinter import *
+
+from setuptools import Command 
+
+root = Tk()
+
+root.title('Abrir Browser')
+root.geometry('300x200')
+
+def google():
+    webbrowser.open('www.google.com')
+
+mygoogle = Button(root, text = 'Abrir o Google', command=google).pack(pady=20)
+root.mainloop()
