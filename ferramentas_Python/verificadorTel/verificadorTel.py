900609935b0cc7e4253436f07467aa40f7b7e110
jrrodrigo421 <rjr89000@gmail.com>
2022-07-18 11:12:33 -0300

    testes com a lib

--- ferramentas_Python/verificadorTel/verificadorTel.py
+++ ferramentas_Python/verificadorTel/verificadorTel.py
@@ -1,12 +1,19 @@
 import phonenumbers
+from phonenumbers import timezone
 
-from phonenumbers import geocoder
+phonenumber = phonenumbers.parse('+553534712908')
+timezone = timezone.time_zones_for_number(phonenumber)
 
-phone = input('Digite  o telefone no formato: +5535900000000: \n')
+print(timezone)
 
-phone_number = phonenumbers.parse(phone)
 
-print(geocoder.description_for_number(phone_number, 'pt'))
+# from phonenumbers import geocoder
+
+# phone = input('Digite  o telefone no formato: +5535900000000: \n')
+
+# phone_number = phonenumbers.parse(phone)
+
+# print(geocoder.description_for_number(phone_number, 'pt'))
 
 
 
